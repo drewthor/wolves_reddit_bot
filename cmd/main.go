2012c56d2a18a5cce2468adb6229eb7f310fc8a6
Drew Turnblad <drew@turnblad.me>
2019-10-09 14:27:46 -0500

    fix build

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,13 @@
 package main
 
 import (
-	"sync"
-
 	"github.com/drewthor/wolves_reddit_bot/apis/nba"
 	"github.com/drewthor/wolves_reddit_bot/pkg/gfunctions/gt"
 )
 
 func main() {
-	var wg sync.WaitGroup
-	wg.Add(1)
-	gt.CreateGameThread(nba.MinnesotaTimberwolves, &wg)
-	wg.Wait()
+	//var wg sync.WaitGroup
+	//wg.Add(1)
+	gt.CreateGameThread(nba.MinnesotaTimberwolves /*, &wg*/)
+	//wg.Wait()
 }
